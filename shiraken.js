ac830c896c670deeffcbcd3464f446fa9f622628
あんりーだぶる <wefdjkre@gmail.com>
2022-07-03 17:19:03 +0900

    add:スクロールトリガーの追加 付けたり外したりするcssは仮のもの

--- shiraken.js
+++ shiraken.js
@@ -1,4 +1,26 @@
-$()
-{
-    console.log("jquery installed check");
-}
+$(function () {
+    $(window).on('scroll', function () {
+        function edit($target, classes) {
+            if (scroll > heroImageHeight * 0.7) {
+                $target.addClass(classes);
+            } else {
+                $target.removeClass(classes);
+            }
+        }
+
+        heroimage = $("#heroimage")
+        let scroll = $(window).scrollTop(), //スクロール位置を取得
+            //            triggerTop = $(heroimage).offset().top,  //指定要素のY座標
+            heroImageHeight = $(heroimage).height();    //ウィンドウの高さ
+        //スクロールする度に判定以上のことをしてるのが気になる
+        //---
+        //スクロールによって変更を加えるcssは以下に記載
+        //---
+        // edit($("header"),"")
+        edit($("header ul"), "flex");
+
+
+
+
+    });
+});
