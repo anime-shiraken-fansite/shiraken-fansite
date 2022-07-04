4c7b9946731723b3ce80a2c8ef610225431c8302
あんりーだぶる <wefdjkre@gmail.com>
2022-07-04 20:06:27 +0900

    add:仮の画像を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     extend: {
       backgroundImage:{
         danger_stripe:"repeating-linear-gradient(45deg, rgb(234,179,8), rgb(234,179,8) 10% ,gray 10%, gray 20% )",
+        heroimage:"url('./images/heroimage_dummy.png')",
+        dummyicon:"url('./images/icon_dummy.png')",
       }
     },
   },
